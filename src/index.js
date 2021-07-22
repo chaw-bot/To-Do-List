9bb8fe002a2f52249502f605f81807d169e02663
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-22 14:12:35 +0200

    Declare elements and add add function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+const clear = document.querySelector('.clear');
+const check = "fa-check-circle";
+const uncheck = "fa-circle-thin";
+const line_through = "lineThrough";
 
-    return element;
+// Add task to list
+function addToDo(toDo, i, complete, remove) {
+
+  if(remove) {
+    return;
   }
+  const done = complete ? check : uncheck;
+  const line = complete ? line_through : "";
   
-  document.body.appendChild(component());
+  const text = `<li class="task">
+                  <i class="fa ${done} complete" job="complete" aria-hidden="true" id="${i}"></i>
+                  <p class="text" ${line}>${toDo}</p>
+                  <i class="fa fa-trash-o delete" job="delete" aria-hidden="true" id="${i}"></i>
+                </li>`;
+  const position = "beforeend";
+
+  list.insertAdjacentHTML(position, text);
+}
